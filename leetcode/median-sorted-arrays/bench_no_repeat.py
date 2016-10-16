1bbf81bee9d80e021c71b0821c5f8e9993338040
Sergio de Carvalho <sergio.carvalho@workday.com>
2016-10-16 12:15:00 +0100

    Improve solution 3 to "median of sorted arrays"

--- leetcode/median-sorted-arrays/bench_no_repeat.py
+++ leetcode/median-sorted-arrays/bench_no_repeat.py
@@ -4,9 +4,10 @@ from time import time
 from random import randint
 from solution1 import Solution as Solution1
 from solution2 import Solution as Solution2
+from solution3 import Solution as Solution3
 
 
-NUM_TESTS = 5
+NUM_TESTS = 3
 MAX_NUMBER = 5 * 10 ** 7
 NUM_ELEMS_1 = 10 ** 7
 NUM_ELEMS_2 = 10 ** 7 + 1
@@ -20,12 +21,13 @@ def main():
 
     solution_1 = Solution1()
     solution_2 = Solution2()
+    solution_3 = Solution3()
 
     numbers = range(-MAX_NUMBER, MAX_NUMBER + 1)
     array_1 = [0] * NUM_ELEMS_1
     array_2 = [0] * NUM_ELEMS_2
 
-    time_1 = time_2 = 0
+    time_1 = time_2 = time_3 = 0
 
     for _ in xrange(NUM_TESTS):
 
@@ -56,17 +58,24 @@ def main():
         res_2 = solution_2.findMedianSortedArrays(array_1, array_2)
         time_2 += time() - start
 
-        #if res_1 != res_2:
+        assert res_2 == res_1
+
+        #if res_2 != res_1:
         #    print "ERROR: Inconsistency in results."
         #    print "Array 1: %s" % ' '.join([str(x) for x in array_1])
         #    print "Array 2: %s" % ' '.join([str(x) for x in array_2])
         #    print "Solution 1: %d" % res_1
         #    print "Solution 2: %d" % res_2
 
-        assert res_1 == res_2
+        start = time()
+        res_3 = solution_3.findMedianSortedArrays(array_1, array_2)
+        time_3 += time() - start
+
+        assert res_3 == res_1
 
     print "Solution 1: %.2f seconds" % (time_1 / NUM_TESTS)
     print "Solution 2: %.2f seconds" % (time_2 / NUM_TESTS)
+    print "Solution 3: %.2f seconds" % (time_3 / NUM_TESTS)
 
 
 if __name__ == '__main__':
--- leetcode/median-sorted-arrays/solution3.py
+++ leetcode/median-sorted-arrays/solution3.py
@@ -2,8 +2,6 @@
 #
 # Iterate over the sorted arrays until the middle point is reached.
 
-from heapq import merge
-
 class Solution(object):
 
     def findMedianSortedArrays(self, nums1, nums2):
@@ -13,22 +11,54 @@ class Solution(object):
         :rtype: float
         """
 
-        total_len = len(nums1) + len(nums2)
-        if total_len <= 0:
+        len1 = len(nums1)
+        len2 = len(nums2)
+        total_len = len1 + len2
+
+        if total_len == 0:
             return None
 
-        iterator = merge(nums1, nums2)
+        median = pos1 = pos2 = 0
 
-        if total_len == 1:
-            return iterator.next()
+        phase_1 = min((total_len - 1) / 2, len1, len2)
 
-        for _ in xrange(total_len / 2 - 1):
-            iterator.next()
+        for _ in xrange(phase_1):
+            if nums1[pos1] < nums2[pos2]:
+                pos1 += 1
+            else:
+                pos2 += 1
 
-        mid_1 = iterator.next()
-        mid_2 = iterator.next()
+        phase_2 = ((total_len - 1) / 2) - phase_1
+
+        for _ in xrange(phase_2):
+            if pos1 < len1 and pos2 < len2:
+                if nums1[pos1] < nums2[pos2]:
+                    pos1 += 1
+                else:
+                    pos2 += 1
+            elif pos1 < len1:
+                pos1 += 1
+            else:
+                pos2 += 1
+
+        phase_3 = 2 if total_len % 2 == 0 else 1
+
+        for _ in xrange(phase_3):
+            if pos1 < len1 and pos2 < len2:
+                if nums1[pos1] < nums2[pos2]:
+                    median += nums1[pos1]
+                    pos1 += 1
+                else:
+                    median += nums2[pos2]
+                    pos2 += 1
+            elif pos1 < len1:
+                median += nums1[pos1]
+                pos1 += 1
+            else:
+                median += nums2[pos2]
+                pos2 += 1
 
         if total_len % 2 == 0:
-            return (mid_1 + mid_2) / 2.0
+            return median / 2.0
         else:
-            return mid_2
+            return median
